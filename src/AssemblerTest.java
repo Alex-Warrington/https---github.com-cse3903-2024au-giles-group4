2e3405948a02ab8a68b89b0425614ed5903ddb71
Alex Warrington <warrington.21@osu.edu>
2024-10-14 16:58:21 -0400

    init

---
+++ src/AssemblerTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AssemblerTest {
+
+    @Test
+    public void test() {
+        assertEquals(2, 1+1);
+    }
+
+
+}
